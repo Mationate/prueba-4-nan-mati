72b18ee48ea894687bc51a1af3e7cc88545395fe
Matias Oñate <mationate.0@gmail.com>
2019-02-14 11:34:12 -0300

    show and index views on todo model working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+9.times do |i|
+  Todo.create(
+    name: "Tarea#{i}",
+    image: "https://picsum.photos/400/400/?image=#{rand(10..30)}"
+  )
+end

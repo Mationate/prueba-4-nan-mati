4f0e00b25f429594a5cb0670d350d4305d1cc453
Matias Oñate <mationate.0@gmail.com>
2019-02-14 16:55:51 -0300

    fix minor things

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :todos, only: %i[index show] do
-    resources :tasks, only: %i[create update]
+    resources :tasks, only: %i[create]
     delete 'uncompleted', to: 'tasks#destroy'
   end
   root 'todos#index'
